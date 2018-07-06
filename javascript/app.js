8603ef78f902eeb20b102e125079e3c078f48006
KKT84 <35786891+kthoma2484@users.noreply.github.com>
2018-07-05 22:37:40 -0700

    added additional js files to run each page function

--- javascript/app.js
+++ javascript/app.js
@@ -3,82 +3,57 @@ $(function () {
 
     // -----------------------Kim's Section Starts------------------------  
     let dessertInput = "";
-    let dessertName = "";
-    let ingredients = "";
-    let dessertTime = "";
-    let criteriaAdded = "";
+    let themeSong = document.getElementById('page-audio');
 
-    function recipeSearch() {
-        console.log(dessertInput + "this is search term")
-        let queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=d12c5941&_app_key=608774bd91d1850727be4bd6a7ba4dde&q=";
-
-        $.ajax({
-                // make ajax GET request
-                url: queryURL + encodeURIComponent("pie") + "&allowedCourse[]=course^course-Desserts&maxResult=20",
-                method: "GET"
-            })
-            .then(function (response) {
-
-                console.log(response)
-
-                for (let i=0; i < response.matches.length; i++) {
-
-                // store the needed results data
-                dessertName = response.matches[i].recipeName
-                rating = response.matches[i].rating
-                ingredients = response.matches[i].ingredients
-                dessertTime = moment.utc(parseInt(response.matches[i].totalTimeInSeconds) * 1000).format("HH:mm");
-                dessertPhoto = response.matches[i].imageUrlsBySize[90]
-
-                if (parseInt(response.matches[i].rating) >= 4) {
-                        console.log("this is highly rated = " + dessertName);
-
-                    $("#results-table > tbody").append(`
-                    <tr id="${dessertName}">
-                    <td> ${dessertName}  </td>
-                    <td>  ${rating}  </td>
-                    <td> ${dessertTime} </td>
-                    <td> <img src="${dessertPhoto}" class="zoom" data-caption="${dessertName}"></td>`)
-                }
-                 
-                else if (response.matches[i].attributes.course[0] !== "Desserts") {
-                    $("results-table").remove();
-                    $(".no-results").append(`<p> Sorry. We couldn't find any results related to your search. Please press 'reset' and try another search.</p>`)
-                }
-
-                 }
-            });
-    }
-
-
-    let categorySearch = function() {
-        
-    }
-
-    // Submit either the typed search term(s) or the criteria selections
-    $("#submit").on("click", function() {
-        event.preventDefault();
-        criteriaAdded = $("#dessert-name").val().trim();
-        console.log(criteriaAdded)
+    // audio to play/pause music buttons
+    $('.play-audio').click(function () {
+        themeSong.play();
     });
 
+    $('.pause-audio').click(function () {
+        themeSong.pause();
+    });
 
-    //recipeSearch();
+    // validate search form entry
+    function validateForm() {
+        let x = $("[name='dessert']").val();
+        if (x == "") {
+            console.log("No text entered");
+        }
+    }
+
+    // Selet to enter search text or use radio criteria for search
+    $(".form-check-input").on('click', function () {
+        themeSong.play();
+        // make visible the search term input box
+        if ($("#enter-name").is(':checked')) {
+            $(".option-one").removeAttr("id");
+            $(".submit-div").removeAttr("id");
+            $("#intro-question").attr("id", "hide-one");
+        }
+        // make visiable the criteria radio questions & buttons
+        if ($("#criteria-search").is(':checked')) {
+            $(".option-two").removeAttr("id");
+            $(".submit-div").removeAttr("id");
+            $("#intro-question").attr("id", "hide-one");
+        }
+    });
+
     // ----------------------Kim's Section Ends----------------------------------
 
-/*
-    // ----------------------Anthony's Section Starts----------------------------
-    let queryURL = "https://www.googleapis.com/youtube/v3/AIzaSyBqsfshUVe2dhIcYn_bKTXrpr9Pst1g3Vo"
+    /*
+        // ----------------------Anthony's Section Starts----------------------------
+        let queryURL = "https://www.googleapis.com/youtube/v3/AIzaSyBqsfshUVe2dhIcYn_bKTXrpr9Pst1g3Vo"
 
-    $.ajax({
-        url: "https://www.googleapis.com/youtube/v3/AIzaSyBqsfshUVe2dhIcYn_bKTXrpr9Pst1g3Vo",
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    })
-*/
+        $.ajax({
+            url: "https://www.googleapis.com/youtube/v3/AIzaSyBqsfshUVe2dhIcYn_bKTXrpr9Pst1g3Vo",
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        })
+    */
 
 
-    
+
 
 });
---
+++ javascript/app3.js
@@ -0,0 +1,98 @@
+$(function () {
+
+
+    // -----------------------Kim's Section Starts------------------------  
+    let dessertInput = "";
+    let dessertName = "";
+    let ingredients = "";
+    let dessertTime = "";
+    let dessertId = "";
+    let themeSong = document.getElementById('page-audio');
+
+    // audio to play/pause music buttons
+    $('.play-audio').click(function () {
+        themeSong.play();
+    });
+
+    $('.pause-audio').click(function () {
+        themeSong.pause();
+    });
+
+    // api pull request from yummly api
+    function recipeSearch() {
+
+        console.log(dessertId + " = is search term")
+        let queryURL = "http://api.yummly.com/v1/api/recipe/";
+
+        $.ajax({
+                // make ajax GET request
+                url: queryURL + encodeURI(dessertId) + "?_app_id=d12c5941&_app_key=608774bd91d1850727be4bd6a7ba4dde",
+                method: "GET",
+            })
+            .then(function (response) {
+                console.log(response)
+
+
+                // store the needed results data
+                dessertId = response.id;
+                dessertName = response.name;
+                ingredients = response.ingredientLines;
+                recipeLink = response.attribution.url;
+                dessertTime = moment.utc(parseInt(response.totalTimeInSeconds) * 1000).format("HH:mm");
+
+                $(".single-pick").html(dessertName);
+                $("#ingredients").html(ingredients);
+                $("#recipe-link").html(`<a href="${recipeLink}">${recipeLink}</a>`);
+
+            });
+    };
+
+    // Read a page's GET URL variables and return them as an associative array.
+    function getUrlVars() {
+        let vars = [],
+            hash;
+        let hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for (let i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    }
+    dessertId = getUrlVars()["id"];
+    recipeSearch();
+
+    // reset to main page and clear all values
+    function reset() {
+        window.location = "index.html";
+        $("#dessert-name").empty();
+        $(".form-check-input").prop("checked", false);
+        $("tbody").empty();
+        $(".single-pick").empty();
+        $("#ingredients").empty();
+        $("#recipe-link").empty();
+        $(".video").empty();
+        themeSong.pause();
+    }
+
+    $("#reset").on("click", function () {
+        reset();
+    });
+    // ----------------------Kim's Section Ends----------------------------------
+
+    /*
+        // ----------------------Anthony's Section Starts----------------------------
+        let queryURL = "https://www.googleapis.com/youtube/v3/AIzaSyBqsfshUVe2dhIcYn_bKTXrpr9Pst1g3Vo"
+
+        $.ajax({
+            url: "https://www.googleapis.com/youtube/v3/AIzaSyBqsfshUVe2dhIcYn_bKTXrpr9Pst1g3Vo",
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        })
+    */
+
+
+
+
+});
